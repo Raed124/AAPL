b6e8774b96b4aa0487fc170b7d4ee9fe4e6b231d
Triumph <triumph@gmail.com>
2024-07-13 19:56:12 -0400

    integrated mlflow , cross validation , nad saved the best experiment

--- main.py
+++ main.py
@@ -1,38 +1,92 @@
 import pandas as pd 
 import numpy as np  
 from matplotlib import pyplot
-from sklearn.metrics import mean_absolute_error
+import json 
+import mlflow
+#setting the tracking uri  
+mlflow.set_tracking_uri(uri="http://127.0.0.1:8080")
+mlflow.set_experiment("MLflow Quickstart")
+from mlflow.models import infer_signature
+
+#reading the datset 
 stockprice = pd.read_csv("Dataset3.csv",parse_dates=['TimeStamp'])
 stockprice = stockprice.rename(columns={'TimeStamp': 'ds','close': 'y'})
+print(stockprice.shape)
+fig2 = stockprice.plot(kind='scatter',
+        x='ds',
+        y='y',
+        color='red')
+#the artifact path 
+ARTIFACT_PATH = "model"
+np.random.seed(12345)
+
+#a function to extract the parameters of the model 
+def extract_params(pr_model):
+    return {attr: getattr(pr_model, attr) for attr in serialize.SIMPLE_ATTRIBUTES}
 
 
+"""#train test split 
 threshold = int((0.2)*len(stockprice))
 stock_test = stockprice.iloc[:threshold,]
-stock_train = stockprice.iloc[threshold:]
+stock_train = stockprice.iloc[threshold:]"""
 
 
 
-from prophet import Prophet
-model = Prophet(daily_seasonality = True)
-
-model.fit(stock_train)
-
-future = model.make_future_dataframe(periods=1241,include_history = False)
-#future.tail()
+from prophet import Prophet, serialize
+from prophet.diagnostics import cross_validation, performance_metrics
 
 
+with mlflow.start_run():
+    model = Prophet().fit(stockprice)
+
+    params = extract_params(model)
+
+    metric_keys = ["mse", "rmse", "mae", "mape", "mdape", "smape", "coverage"]
+
+    #cross validation 
+    metrics_raw = cross_validation(
+        model=model,
+        horizon="365 days",
+        period="180 days",
+        initial="900 days",
+        parallel="threads",
+        disable_tqdm=True,
+    )
+    cv_metrics = performance_metrics(metrics_raw)
+    metrics = {k: cv_metrics[k].mean() for k in metric_keys}
+
+    print(f"Logged Metrics: \n{json.dumps(metrics, indent=2)}")
+    print(f"Logged Params: \n{json.dumps(params, indent=2)}")
+
+    train = model.history
+    #print(train.shape())
+    predictions = model.predict(model.make_future_dataframe(30))
+    signature = infer_signature(train, predictions)
+
+    mlflow.prophet.log_model(model, artifact_path=ARTIFACT_PATH, signature=signature)
+    Path = './Models/'
+    mlflow.prophet.save_model(model,Path)
+    mlflow.log_params(params)
+    mlflow.log_metrics(metrics)
+    model_uri = mlflow.get_artifact_uri(ARTIFACT_PATH)
+    #model_uri = 'models/'
+    print(f"Model artifact logged to: {model_uri}")
 
 
-forecast = model.predict(future)
-#forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
-print(len(forecast))
+#loaded_model = mlflow.prophet.load_model(model_uri)
+
+loaded_model_2 = mlflow.prophet.load_model(Path)
+
+forecast = loaded_model_2.predict(loaded_model_2.make_future_dataframe(60,include_history = False))
+
+print(f"forecast:\n${forecast.head(30)}")
+
+
+#include_history = False
+
 fig1 = model.plot(forecast)
 
 pyplot.show()
 
 # calculate MAE between expected and predicted values for the test set 
-y_true = stock_test['y'].values
-y_pred = forecast['yhat'].values
-mae = mean_absolute_error(y_true, y_pred)
-print('MAE: %.3f' % mae)
 #MAE = 72.252
